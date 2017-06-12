3ccdc2604178ee9c74c64bfce5a8afa4053aef7e
Albert Julius Liu <ajul1987@gmail.com>
2017-06-12 19:01:10 -0400

    remove simple multiplicative example

--- python/examples/pokemon.py
+++ python/examples/pokemon.py
@@ -7,12 +7,8 @@ import zerosum.nash
 import matplotlib
 import matplotlib.pyplot as plt
 
-figsize = (12, 6)
-dpi = 240
-marker_size = 64
-text_size = 15
-
-loc = matplotlib.ticker.MultipleLocator(base=0.1)
+# plots handicaps (using a multiplicative handicap function)
+# versus Nash equilibrium of the original game
 
 data = dataset.nonsymmetric.pokemon_type_chart_6
 names = dataset.nonsymmetric.pokemon_type_names_6
@@ -21,13 +17,17 @@ colors = [dataset.nonsymmetric.pokemon_type_colors[name] for name in names]
 row_nash, col_nash = zerosum.nash.nash(data)
 balance = zerosum.balance.MultiplicativeBalance(data).optimize()
 
-# by attacker and defender
+dpi = 240
+marker_size = 64
+text_size = 15
+loc = matplotlib.ticker.MultipleLocator(base=0.1)
 
+# plot attacker and defender on separate subplots
 fig = plt.figure(figsize = (12, 7))
 gs = matplotlib.gridspec.GridSpec(1, 2, width_ratios=[2, 3])
 ax = plt.subplot(gs[0])
 
-# uniform line
+# draw a line indicating where the uniform distribution would be
 uniform_x = 1.0 / len(row_nash.strategy)
 ax.plot([uniform_x, uniform_x], [-0.2, 0.25],
         color='#bfbfbf', linestyle='-', zorder=0)
@@ -36,13 +36,16 @@ plt.text(uniform_x, -0.203, 'Uniform',
          rotation = 0,
          ha = 'center', va = 'top')
 
+# scatter plot of handicaps vs. nash
 x = row_nash.strategy
-y = numpy.log(balance.row_handicaps)
+y = balance.row_log_handicaps
 
 ax.scatter(x, y, s = marker_size, c = colors)
 
+# label each scatter plot point with the type name
 for pointx, pointy, name, color in zip(x, y, names, colors):
     ha = 'left'
+    # manual adjustment
     if name in ['Flying']:
         pointy -= 0.005
     if name in ['Dark']:
@@ -69,14 +72,9 @@ ax.set_ylabel('Log (handicap) producing uniform Nash', fontsize = text_size)
 ax.set_xlim(left=0.0, right = 0.2)
 ax.set_ylim(bottom=-0.2, top = 0.25)
 
-#plt.savefig("out/pokemon_scatter_attacker.png", dpi = dpi, bbox_inches = "tight")
-#plt.close()
-
-
-#fig = plt.figure(figsize = figsize)
+# now for the defender plot
 ax = plt.subplot(gs[1])
 
-# uniform line
 uniform_x = 1.0 / len(col_nash.strategy)
 ax.plot([uniform_x, uniform_x], [-0.2, 0.25],
         color='#bfbfbf', linestyle='-', zorder=0)
@@ -86,7 +84,7 @@ plt.text(uniform_x, -0.203, 'Uniform',
          ha = 'center', va = 'top')
 
 x = col_nash.strategy
-y = numpy.log(balance.col_handicaps)
+y = balance.col_log_handicaps
 
 ax.scatter(x, y, s = marker_size, c = colors)
 
@@ -112,7 +110,6 @@ ax.yaxis.set_major_locator(loc)
 ax.set_title('Defender')
 
 ax.set_xlabel('Nash probability of initial game', fontsize = text_size)
-# ax.set_ylabel('Log (handicap) producing uniform Nash')
 ax.yaxis.set_ticklabels([])
 
 ax.set_xlim(left=0.0, right = 0.3)
@@ -120,8 +117,6 @@ ax.set_ylim(bottom=-0.2, top = 0.25)
 
 plt.tight_layout()
 
-#plt.savefig("out/pokemon_scatter_defender.png", dpi = dpi, bbox_inches = "tight")
-#plt.close()
-
 plt.savefig("out/pokemon_scatter.png", dpi = dpi, bbox_inches = "tight")
-plt.close()
+plt.show()
+
