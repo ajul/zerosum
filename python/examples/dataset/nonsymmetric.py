d1eab680ee4fcb155df6ffad18def4717f45a6a2
Albert Julius Liu <ajul1987@gmail.com>
2017-06-12 18:44:37 -0400

    add pokemon colors

--- python/examples/dataset/nonsymmetric.py
+++ python/examples/dataset/nonsymmetric.py
@@ -76,3 +76,24 @@ pokemon_type_chart_6 = NonSymmetricDataset(
         [1.0, 0.5, 1.0, 1.0, 1.0, 1.0, 1.0, 2.0, 1.0, 1.0, 1.0, 1.0, 1.0, 2.0, 1.0, 1.0, 0.5, 0.5,],
         [1.0, 2.0, 1.0, 0.5, 1.0, 1.0, 1.0, 1.0, 0.5, 0.5, 1.0, 1.0, 1.0, 1.0, 1.0, 2.0, 2.0, 1.0,],
         ]), pokemon_type_names_6)
+        
+pokemon_type_colors = {
+    'Normal' : '#A8A878',
+    'Fighting' : '#C03028',
+    'Flying' : '#A890F0',
+    'Poison' : '#A040A0',
+    'Ground' : '#E0C068',
+    'Rock' : '#B8A038',
+    'Bug' : '#A8B820',
+    'Ghost' : '#705898',
+    'Steel' : '#B8B8D0',
+    'Fire' : '#F08030',
+    'Water' : '#6890F0',
+    'Grass' : '#78C850',
+    'Electric' : '#F8D030',
+    'Psychic' : '#F85888',
+    'Ice' : '#98D8D8',
+    'Dragon' : '#7038F8',
+    'Dark' : '#705848',
+    'Fairy' : '#EE99AC',
+    }
