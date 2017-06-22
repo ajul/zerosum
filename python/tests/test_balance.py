d04f03b477991005161c473e0af31bc542860aeb
ajul <ajul1987@gmail.com>
2017-06-22 00:54:12 -0400

    add more logistic tests

--- python/tests/test_balance.py
+++ python/tests/test_balance.py
@@ -10,6 +10,8 @@ class TestMultiplicativeBalance(unittest.TestCase):
             zerosum.balance.MultiplicativeBalance(data)
 
 class TestLogisticSymmetricBalance(unittest.TestCase):
+    strategy_count = 16
+
     def test_non_skew_symmetric(self):
         data = numpy.eye(2) + 0.5
         with self.assertWarnsRegex(zerosum.balance.InitialPayoffMatrixWarning, 'skew'):
@@ -27,6 +29,34 @@ class TestLogisticSymmetricBalance(unittest.TestCase):
                             [epsilon, 0.5]])
         with self.assertWarnsRegex(zerosum.balance.InitialPayoffMatrixWarning, 'close to'):
             zerosum.balance.LogisticSymmetricBalance(data)
-
+    
+    def test_random_unweighted(self):
+        data = numpy.random.random((self.strategy_count, self.strategy_count))
+        data_nt = 1.0 - data
+        data = 0.5 * (data + data_nt)
+        result = zerosum.balance.LogisticSymmetricBalance(data).optimize()
+        numpy.testing.assert_allclose(numpy.average(result.F, axis = 0), 0.5)
+        numpy.testing.assert_allclose(numpy.average(result.F, axis = 1), 0.5)
+        
+    def test_random_weighted(self):
+        strategy_weights = numpy.random.random((self.strategy_count,))
+        data = numpy.random.random((self.strategy_count, self.strategy_count))
+        data_nt = 1.0 - data
+        data = 0.5 * (data + data_nt)
+        result = zerosum.balance.LogisticSymmetricBalance(data, strategy_weights).optimize()
+        numpy.testing.assert_allclose(numpy.average(result.F, weights = strategy_weights, axis = 0), 0.5)
+        numpy.testing.assert_allclose(numpy.average(result.F, weights = strategy_weights, axis = 1), 0.5)
+    
+    def test_random_weighted_with_zeros(self):
+        strategy_weights = numpy.concatenate((numpy.random.random((self.strategy_count,)),
+                                              numpy.zeros((self.strategy_count,))))
+        numpy.random.shuffle(strategy_weights)
+        data = numpy.random.random((self.strategy_count * 2, self.strategy_count * 2))
+        data_nt = 1.0 - data
+        data = 0.5 * (data + data_nt)
+        result = zerosum.balance.LogisticSymmetricBalance(data, strategy_weights).optimize()
+        numpy.testing.assert_allclose(numpy.average(result.F, weights = strategy_weights, axis = 0), 0.5)
+        numpy.testing.assert_allclose(numpy.average(result.F, weights = strategy_weights, axis = 1), 0.5)
+    
 if __name__ == '__main__':
     unittest.main()
