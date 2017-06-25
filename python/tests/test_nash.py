fe87a407438bde36795aaf467959e06c95cec9b7
ajul <ajul1987@gmail.com>
2017-06-25 01:53:22 -0400

    add nash test

---
+++ python/tests/test_nash.py
@@ -0,0 +1,19 @@
+import numpy
+import unittest
+import zerosum.nash
+
+num_random_trials = 100
+strategy_count = 16
+
+class TestNash(unittest.TestCase):
+    def test_random(self):
+        for i in range(num_random_trials):
+            payoff_matrix = numpy.random.normal(size = (strategy_count, strategy_count))
+            row_result, col_result = zerosum.nash.nash(payoff_matrix)
+            # Both results should have the same value (with opposite sign).
+            numpy.testing.assert_allclose(row_result.value, -col_result.value)
+            row_payoffs = numpy.average(payoff_matrix, axis = 1, weights = col_result.strategy)
+            col_payoffs = numpy.average(payoff_matrix, axis = 0, weights = row_result.strategy)
+            # Value should be close to the best possible expected payoff.
+            numpy.testing.assert_allclose(numpy.max(row_payoffs), row_result.value)
+            numpy.testing.assert_allclose(numpy.min(col_payoffs), -col_result.value)
