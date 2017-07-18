ce996c0a7119e0196de258d5a079b2637562f463
Albert Julius Liu <ajul1987@gmail.com>
2017-07-17 21:31:25 -0400

    start warhammer 40k example

---
+++ python/examples/warhammer_40k.py
@@ -0,0 +1,21 @@
+import _initpath
+
+import numpy
+import dataset.nonsymmetric
+import zerosum.balance
+import zerosum.nash
+import matplotlib
+import matplotlib.pyplot as plt
+
+strength_weights = None # numpy.array([0.0, 1.0, 8.0, 8.0, 4.0, 4.0, 2.0, 2.0, 2.0, 1.0])
+toughness_weights = None # numpy.array([0.0, 1.0, 8.0, 8.0, 2.0, 2.0, 1.0, 1.0, 0.0, 0.0])
+
+# Nash equilibrium of the initial game.
+row_nash_7, col_nash_7 = zerosum.nash.nash(dataset.nonsymmetric.wh40k_7_to_wound)
+# Handicaps producing a uniform Nash equilibrium.
+balance_7 = zerosum.balance.MultiplicativeBalance(dataset.nonsymmetric.wh40k_7_to_wound, strength_weights, toughness_weights).optimize()
+
+# Nash equilibrium of the initial game.
+row_nash_8, col_nash_8 = zerosum.nash.nash(dataset.nonsymmetric.wh40k_8_to_wound)
+# Handicaps producing a uniform Nash equilibrium.
+balance_8 = zerosum.balance.MultiplicativeBalance(dataset.nonsymmetric.wh40k_8_to_wound, strength_weights, toughness_weights).optimize()
