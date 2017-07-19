2ec161edfdf1689e37c253e25938deed9370a0cc
ajul <ajul1987@gmail.com>
2017-07-18 23:38:24 -0400

    csv output for warhammer

--- python/examples/warhammer_40k.py
+++ python/examples/warhammer_40k.py
@@ -19,3 +19,6 @@ balance_7 = zerosum.balance.MultiplicativeBalance(dataset.nonsymmetric.wh40k_7_t
 row_nash_8, col_nash_8 = zerosum.nash.nash(dataset.nonsymmetric.wh40k_8_to_wound)
 # Handicaps producing a uniform Nash equilibrium.
 balance_8 = zerosum.balance.MultiplicativeBalance(dataset.nonsymmetric.wh40k_8_to_wound, strength_weights, toughness_weights).optimize()
+
+dataset.csv.write_csv('out/wh40k_7_to_wound_opt.csv', balance_7.F, [str(i + 1) for i in range(10)], numeric_format = '%0.4f')
+dataset.csv.write_csv('out/wh40k_8_to_wound_opt.csv', balance_8.F, [str(i + 1) for i in range(10)], numeric_format = '%0.4f')
