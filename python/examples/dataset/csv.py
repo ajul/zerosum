6d0f0d2215e17b9260d47341f89692137e7fa400
Albert Julius Liu <ajul1987@gmail.com>
2017-07-19 18:55:57 -0400

    output handicaps to csv

--- python/examples/dataset/csv.py
+++ python/examples/dataset/csv.py
@@ -1,10 +1,15 @@
 import csv
 
-def write_csv(filename, data, row_headers, col_headers = None, numeric_format = '%f', *args, **kwargs):
+def write_csv(filename, data, row_headers, col_headers = None, row_footers = None, col_footers = None, numeric_format = '%f', *args, **kwargs):
     def iter_rows():
         yield [''] + col_headers
         for row_index in range(data.shape[0]):
-            yield [row_headers[row_index]] + [(numeric_format % x) for x in data[row_index, :]]
+            result = [row_headers[row_index]] + [(numeric_format % x) for x in data[row_index, :]]
+            if row_footers is not None:
+                result.append(numeric_format % row_footers[row_index])
+            yield result
+        if col_footers is not None:
+            yield [''] + [numeric_format % col_footer for col_footer in col_footers]
     if col_headers is None: col_headers = row_headers
     with open(filename, 'w', newline='') as outfile:
         writer = csv.writer(outfile, *args, **kwargs)
--- python/examples/warhammer_40k.py
+++ python/examples/warhammer_40k.py
@@ -20,5 +20,13 @@ row_nash_8, col_nash_8 = zerosum.nash.nash(dataset.nonsymmetric.wh40k_8_to_wound
 # Handicaps producing a uniform Nash equilibrium.
 balance_8 = zerosum.balance.MultiplicativeBalance(dataset.nonsymmetric.wh40k_8_to_wound, strength_weights, toughness_weights).optimize()
 
-dataset.csv.write_csv('out/wh40k_7_to_wound_opt.csv', balance_7.F, [str(i + 1) for i in range(10)], numeric_format = '%0.4f')
-dataset.csv.write_csv('out/wh40k_8_to_wound_opt.csv', balance_8.F, [str(i + 1) for i in range(10)], numeric_format = '%0.4f')
+dataset.csv.write_csv('out/wh40k_7_to_wound_opt.csv',
+                      balance_7.F, [str(i + 1) for i in range(10)],
+                      row_footers = balance_7.row_handicaps / balance_7.row_handicaps[0],
+                      col_footers = balance_7.col_handicaps / balance_7.col_handicaps[0],
+                      numeric_format = '%0.4f')
+dataset.csv.write_csv('out/wh40k_8_to_wound_opt.csv',
+                      balance_8.F, [str(i + 1) for i in range(10)],
+                      row_footers = balance_8.row_handicaps / balance_8.row_handicaps[0],
+                      col_footers = balance_8.col_handicaps / balance_8.col_handicaps[0],
+                      numeric_format = '%0.4f')
