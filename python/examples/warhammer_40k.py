d38b0113afe8fe950433a7779d38e17bbbdc0836
ajul <ajul1987@gmail.com>
2017-07-22 11:38:24 -0400

    remove nash, add weighted to wh40k example

--- python/examples/warhammer_40k.py
+++ python/examples/warhammer_40k.py
@@ -7,18 +7,8 @@ import zerosum.nash
 import matplotlib
 import matplotlib.pyplot as plt
 
-strength_weights = None # numpy.array([0.0, 1.0, 8.0, 8.0, 4.0, 4.0, 2.0, 2.0, 2.0, 1.0])
-toughness_weights = None # numpy.array([0.0, 1.0, 8.0, 8.0, 2.0, 2.0, 1.0, 1.0, 0.0, 0.0])
-
-# Nash equilibrium of the initial game.
-row_nash_7, col_nash_7 = zerosum.nash.nash(dataset.nonsymmetric.wh40k_7_to_wound)
-# Handicaps producing a uniform Nash equilibrium.
-balance_7 = zerosum.balance.MultiplicativeBalance(dataset.nonsymmetric.wh40k_7_to_wound, strength_weights, toughness_weights).optimize()
-
-# Nash equilibrium of the initial game.
-row_nash_8, col_nash_8 = zerosum.nash.nash(dataset.nonsymmetric.wh40k_8_to_wound)
-# Handicaps producing a uniform Nash equilibrium.
-balance_8 = zerosum.balance.MultiplicativeBalance(dataset.nonsymmetric.wh40k_8_to_wound, strength_weights, toughness_weights).optimize()
+balance_7 = zerosum.balance.MultiplicativeBalance(dataset.nonsymmetric.wh40k_7_to_wound).optimize()
+balance_8 = zerosum.balance.MultiplicativeBalance(dataset.nonsymmetric.wh40k_8_to_wound).optimize()
 
 dataset.csv.write_csv('out/wh40k_7_to_wound_init.csv',
                       dataset.nonsymmetric.wh40k_7_to_wound,
@@ -37,4 +27,15 @@ dataset.csv.write_csv('out/wh40k_8_to_wound_opt.csv',
                       balance_8.F, [str(i + 1) for i in range(10)],
                       row_footers = balance_8.row_handicaps / balance_8.row_handicaps[0],
                       col_footers = balance_8.col_handicaps / balance_8.col_handicaps[0],
+                      numeric_format = '%0.4f')
+
+strength_weights = numpy.array([0.0, 0.0, 2.0, 2.0, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0])
+toughness_weights = strength_weights
+
+balance_7_weighted = zerosum.balance.MultiplicativeBalance(dataset.nonsymmetric.wh40k_7_to_wound, strength_weights, toughness_weights).optimize()
+
+dataset.csv.write_csv('out/wh40k_7_to_wound_weighted_opt.csv',
+                      balance_7_weighted.F, [str(i + 1) for i in range(10)],
+                      row_footers = balance_7_weighted.row_handicaps / balance_7_weighted.row_handicaps[0],
+                      col_footers = balance_7_weighted.col_handicaps / balance_7_weighted.col_handicaps[0],
                       numeric_format = '%0.4f')
