f6c6dc5bcb7a3c4b15d88580393c617e0bda9541
Albert Julius Liu <ajul1987@gmail.com>
2017-08-15 16:41:21 -0400

    reinstate nonsymmetric tests, fix them by adding value to kwargs

--- python/tests/test_balance_hazard.py
+++ python/tests/test_balance_hazard.py
@@ -5,18 +5,17 @@ import timeit
 import zerosum.balance
 import tests.common
 
-"""
 class TestHazardNonSymmetricBalance(tests.common.TestInitialMatrixNonSymmetricBalanceBase):
     class_to_test = zerosum.balance.HazardNonSymmetricBalance
     
     def generate_random_args(self, rows, cols):
-        value = numpy.random.rand() - 0.5
+        value = 1.0 * (numpy.random.rand() - 0.5)
         initial_payoff_matrix = 0.1 + numpy.random.random((rows, cols))
         kwargs = {
             'initial_payoff_matrix' : initial_payoff_matrix,
+            'value' : value,
         }
         return kwargs, value
-"""
 
 class TestHazardSymmetricBalance(tests.common.TestInitialMatrixSymmetricBalanceBase):
     class_to_test = zerosum.balance.HazardSymmetricBalance
--- python/tests/test_balance_lanchester.py
+++ python/tests/test_balance_lanchester.py
@@ -5,7 +5,6 @@ import timeit
 import zerosum.balance
 import tests.common
 
-"""
 class TestLanchesterNonSymmetricBalance(tests.common.TestInitialMatrixNonSymmetricBalanceBase):
     class_to_test = zerosum.balance.LanchesterNonSymmetricBalance
     
@@ -14,9 +13,9 @@ class TestLanchesterNonSymmetricBalance(tests.common.TestInitialMatrixNonSymmetr
         initial_payoff_matrix = 0.1 + numpy.random.random((rows, cols))
         kwargs = {
             'initial_payoff_matrix' : initial_payoff_matrix,
+            'value' : value,
         }
         return kwargs, value
-"""
 
 class TestLanchesterSymmetricBalance(tests.common.TestInitialMatrixSymmetricBalanceBase):
     class_to_test = zerosum.balance.LanchesterSymmetricBalance
