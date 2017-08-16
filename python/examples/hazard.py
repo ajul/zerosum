8794765cd488d2fb6eff5ebc55d63ab751672cad
Albert Julius Liu <ajul1987@gmail.com>
2017-08-16 18:45:58 -0400

    better comments, formatting for hazard example

--- python/examples/hazard.py
+++ python/examples/hazard.py
@@ -7,20 +7,25 @@ import zerosum.nash
 import matplotlib
 import matplotlib.pyplot as plt
 
+"""
+Example taken from:
+
+Hazard, C. J. 2010. What every game designer should know about game theory. Triangle Game Conference. Raleigh, North Carolina.
+"""
+
 data = numpy.array([
     [1.0, 3.0, 0.5],
     [1.0 / 3.0, 1.0, 0.5],
     [2.0, 2.0, 1.0]])
 
-# change convention
+# We use the opposite convention from the original presentation:
+# We treat higher as better for the row player.
 data = data.transpose()
 
 names = ['Hammer', 'Spear', 'Curse']
 
-
 balance = zerosum.balance.HazardSymmetricBalance(data)
-result = balance.optimize(check_derivative = True)
+result = balance.optimize()
 
-print(result)
-
-print(result.handicaps / numpy.sum(result.handicaps))
+for name, handicap in zip(names, result.handicaps / numpy.sum(result.handicaps)):
+    print("%8s: %0.3f" % (name, handicap))
