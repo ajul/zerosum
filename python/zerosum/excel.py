1e5fae0cdec329ba912a46dc11ad661a9371d343
ajul <ajul1987@gmail.com>
2017-09-02 19:45:06 -0400

    excel integration work

--- python/zerosum/excel.py
+++ python/zerosum/excel.py
@@ -1,3 +1,9 @@
+"""
+Excel integration. Requires xlwings.
+
+WARNING: Return values are dynamic arrays and will overwrite worksheet values without warning!
+"""
+
 import numpy
 import xlwings
 import zerosum.balance
@@ -14,4 +20,55 @@ def nash_weights(payoff_matrix):
     """
     row_result, col_result = zerosum.nash.nash(payoff_matrix)
     return numpy.stack((row_result.strategy, col_result.strategy), axis = 1)
+
+@xlwings.func
+@xlwings.arg('payoff_matrix', numpy.array, ndim=2)
+def nash_value(payoff_matrix):
+    """
+    Computes value of a payoff_matrix.
+    """
+    row_result, _ = zerosum.nash.nash_row(payoff_matrix)
+    return row_result.value
+
+def compute_balance_result(balance_class, *args, **kwargs):
+    """
+    Common result format:
+           | m cols          1 col
+    --------------------------------------
+    n rows | payoff_matrix   row_handicaps
+    1 row  | col_handicaps   blank
+    """
+    balance = balance_class(*args, **kwargs)
+    result = balance.optimize()
     
+    col_handicaps = numpy.reshape(result.col_handicaps, (1, -1))
+    row_handicaps = numpy.reshape(result.row_handicaps, (-1, 1))
+    result = numpy.block([[result.payoff_matrix, row_handicaps],
+                          [col_handicaps, 0.0]])
+    result = result.tolist()
+    result[-1][-1] = None
+    return result
+    
+@xlwings.func
+@xlwings.arg('base_matrix', numpy.array, ndim=2)
+@xlwings.arg('row_weights', numpy.array, ndim=1)
+@xlwings.arg('col_weights', numpy.array, ndim=1)
+@xlwings.ret(expand='table')
+def multiplicative_balance(base_matrix, row_weights = None, col_weights = None, value = 1.0):
+    return compute_balance_result(zerosum.balance.MultiplicativeBalance, base_matrix, row_weights, col_weights, value)
+    
+@xlwings.func
+@xlwings.arg('base_matrix', numpy.array, ndim=2)
+@xlwings.arg('row_weights', numpy.array, ndim=1)
+@xlwings.arg('col_weights', numpy.array, ndim=1)
+@xlwings.ret(expand='table')
+def logistic_non_symmetric_balance(base_matrix, max_payoff, row_weights = None, col_weights = None, value = None):
+    return compute_balance_result(zerosum.balance.LogisticNonSymmetricBalance, base_matrix, max_payoff, row_weights, col_weights, value)
+
+@xlwings.func
+@xlwings.arg('base_matrix', numpy.array, ndim=2)
+@xlwings.arg('strategy_weights', numpy.array, ndim=1)
+@xlwings.ret(expand='table')
+def logistic_symmetric_balance(base_matrix, strategy_weights = None):
+    return compute_balance_result(zerosum.balance.LogisticSymmetricBalance, base_matrix, strategy_weights)
+
