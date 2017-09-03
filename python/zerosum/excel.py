b98c2ce7c956db711d9393480b2d81bcf3fd5e7f
ajul <ajul1987@gmail.com>
2017-09-02 21:45:25 -0400

    more excel bindings

--- python/zerosum/excel.py
+++ python/zerosum/excel.py
@@ -12,23 +12,39 @@ import zerosum.nash
 @xlwings.func
 @xlwings.arg('payoff_matrix', numpy.array, ndim=2)
 @xlwings.ret(expand='table')
-def nash_weights(payoff_matrix):
+def nash(payoff_matrix):
     """
-    Computes Nash equilbrium weights.
-    Results are put into two columns, 
-    the first corresponding to the row player, the second to the column player.
+    Computes Nash equilbrium strategy weights and value.
+    Result format:
+    
+    row value                col value (should be negative of row value)
+    blank                    blank
+    row strategy 0 weight    col strategy 0 weight
+    row strategy 1 weight    col strategy 1 weight
+    ...                      ...
     """
     row_result, col_result = zerosum.nash.nash(payoff_matrix)
-    return numpy.stack((row_result.strategy, col_result.strategy), axis = 1)
-
-@xlwings.func
-@xlwings.arg('payoff_matrix', numpy.array, ndim=2)
-def nash_value(payoff_matrix):
-    """
-    Computes value of a payoff_matrix.
-    """
-    row_result, _ = zerosum.nash.nash_row(payoff_matrix)
-    return row_result.value
+    
+    max_strategy_length = max(row_result.strategy.size, col_result.strategy.size)
+    row_strategy = numpy.zeros((max_strategy_length, 1))
+    row_strategy[:row_result.strategy.size, 0] = row_result.strategy
+    col_strategy = numpy.zeros((max_strategy_length, 1))
+    col_strategy[:col_result.strategy.size, 0] = col_result.strategy
+    
+    result = numpy.block(
+        [[row_result.value, col_result.value],
+         [0.0, 0.0],
+         [row_strategy, col_strategy]])
+    result = result.tolist()
+    # Blank row separating value from weights.
+    result[1][0] = None
+    result[1][1] = None
+    # Blank cells not belonging to a strategy.
+    for i in range(row_result.strategy.size + 2, len(result)):
+        result[i][0] = None
+    for i in range(col_result.strategy.size + 2, len(result)):
+        result[i][1] = None
+    return result
 
 def compute_balance_result(balance_class, *args, **kwargs):
     """
@@ -43,8 +59,9 @@ def compute_balance_result(balance_class, *args, **kwargs):
     
     col_handicaps = numpy.reshape(result.col_handicaps, (1, -1))
     row_handicaps = numpy.reshape(result.row_handicaps, (-1, 1))
-    result = numpy.block([[result.payoff_matrix, row_handicaps],
-                          [col_handicaps, 0.0]])
+    result = numpy.block(
+        [[result.payoff_matrix, row_handicaps],
+         [col_handicaps, 0.0]])
     result = result.tolist()
     result[-1][-1] = None
     return result
@@ -54,15 +71,37 @@ def compute_balance_result(balance_class, *args, **kwargs):
 @xlwings.arg('row_weights', numpy.array, ndim=1)
 @xlwings.arg('col_weights', numpy.array, ndim=1)
 @xlwings.ret(expand='table')
-def multiplicative_balance(base_matrix, row_weights = None, col_weights = None, value = 1.0):
-    return compute_balance_result(zerosum.balance.MultiplicativeBalance, base_matrix, row_weights, col_weights, value)
+def hazard_non_symmetric_balance(base_matrix, row_weights = None, col_weights = None, value = 0.0):
+    return compute_balance_result(zerosum.balance.HazardNonSymmetricBalance, base_matrix, row_weights, col_weights, value)
+    
+@xlwings.func
+@xlwings.arg('base_matrix', numpy.array, ndim=2)
+@xlwings.arg('strategy_weights', numpy.array, ndim=1)
+@xlwings.ret(expand='table')
+def hazard_symmetric_balance(base_matrix, strategy_weights = None):
+    return compute_balance_result(zerosum.balance.HazardSymmetricBalance, base_matrix, strategy_weights)
     
 @xlwings.func
 @xlwings.arg('base_matrix', numpy.array, ndim=2)
 @xlwings.arg('row_weights', numpy.array, ndim=1)
 @xlwings.arg('col_weights', numpy.array, ndim=1)
 @xlwings.ret(expand='table')
-def logistic_non_symmetric_balance(base_matrix, max_payoff, row_weights = None, col_weights = None, value = None):
+def lanchester_non_symmetric_balance(base_matrix, exponent = 1.0, row_weights = None, col_weights = None, value = 0.0):
+    return compute_balance_result(zerosum.balance.LanchesterNonSymmetricBalance, base_matrix, exponent, row_weights, col_weights, value)
+    
+@xlwings.func
+@xlwings.arg('base_matrix', numpy.array, ndim=2)
+@xlwings.arg('strategy_weights', numpy.array, ndim=1)
+@xlwings.ret(expand='table')
+def lanchester_symmetric_balance(base_matrix, exponent = 1.0, strategy_weights = None):
+    return compute_balance_result(zerosum.balance.LanchesterSymmetricBalance, base_matrix, exponent, strategy_weights)
+    
+@xlwings.func
+@xlwings.arg('base_matrix', numpy.array, ndim=2)
+@xlwings.arg('row_weights', numpy.array, ndim=1)
+@xlwings.arg('col_weights', numpy.array, ndim=1)
+@xlwings.ret(expand='table')
+def logistic_non_symmetric_balance(base_matrix, max_payoff, row_weights = None, col_weights = None, value = 0.0):
     return compute_balance_result(zerosum.balance.LogisticNonSymmetricBalance, base_matrix, max_payoff, row_weights, col_weights, value)
 
 @xlwings.func
@@ -72,3 +111,10 @@ def logistic_non_symmetric_balance(base_matrix, max_payoff, row_weights = None, 
 def logistic_symmetric_balance(base_matrix, strategy_weights = None):
     return compute_balance_result(zerosum.balance.LogisticSymmetricBalance, base_matrix, strategy_weights)
 
+@xlwings.func
+@xlwings.arg('base_matrix', numpy.array, ndim=2)
+@xlwings.arg('row_weights', numpy.array, ndim=1)
+@xlwings.arg('col_weights', numpy.array, ndim=1)
+@xlwings.ret(expand='table')
+def multiplicative_balance(base_matrix, row_weights = None, col_weights = None, value = 1.0):
+    return compute_balance_result(zerosum.balance.MultiplicativeBalance, base_matrix, row_weights, col_weights, value)
