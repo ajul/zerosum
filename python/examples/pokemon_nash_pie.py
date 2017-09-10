f643782d52a98d51d59bc70d9111bb5bd740cfc8
ajul <ajul1987@gmail.com>
2017-09-09 21:29:45 -0400

    add pie chart for pokemon nash

---
+++ python/examples/pokemon_nash_pie.py
@@ -0,0 +1,35 @@
+import _initpath
+
+import numpy
+import dataset.pokemon
+import zerosum.balance
+import zerosum.nash
+import matplotlib
+import matplotlib.pyplot as plt
+
+type_chart = dataset.pokemon.pokemon_6
+# Vector of color codes for the types.
+colors = [dataset.pokemon.pokemon_type_colors[name] for name in type_chart.row_names]
+
+row_nash, col_nash = zerosum.nash.nash(type_chart.data)
+
+dpi = 120
+figsize = (12, 6)
+
+fig, (ax1, ax2) = plt.subplots(1, 2, figsize = figsize, dpi = dpi)
+
+labels = [(name if weight > 0.0 else '') for (name, weight)
+          in zip(type_chart.row_names, row_nash.strategy)]
+ax1.pie(row_nash.strategy, labels = labels, colors = colors)
+ax1.axis('equal')
+ax1.set_title('Attacker')
+
+labels = [(name if weight > 0.0 else '') for (name, weight)
+          in zip(type_chart.col_names, col_nash.strategy)]
+ax2.pie(col_nash.strategy, labels = labels, colors = colors)
+ax2.axis('equal')
+ax2.set_title('Defender')
+
+fig.subplots_adjust(wspace=0.5)
+plt.savefig("out/pokemon_nash_pie.png", dpi = dpi, bbox_inches = "tight")
+plt.show()
