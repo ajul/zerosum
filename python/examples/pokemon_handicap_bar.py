44efab4985fc6186aba0a338f4f9eeea742b53e4
ajul <ajul1987@gmail.com>
2017-09-11 02:02:41 -0400

    improve bar plot spacing

--- python/examples/pokemon_handicap_bar.py
+++ python/examples/pokemon_handicap_bar.py
@@ -16,18 +16,19 @@ balance_result = zerosum.balance.MultiplicativeBalance(type_chart.data).optimize
 
 dpi = 120
 figsize = (12, 6)
-bar_width = 1.0
+bar_width = 0.8
+bar_offset = 0.5 * (1.0 - bar_width)
 
 fig, (ax1, ax2) = plt.subplots(2, 1, figsize = figsize, dpi = dpi)
 x = numpy.arange(len(balance_result.row_handicaps))
 
-ax1.bar(x, numpy.log(balance_result.row_handicaps), color = colors)
+ax1.bar(x + bar_offset, numpy.log(balance_result.row_handicaps), bar_width, color = colors)
 ax1.set_xticks([])
-#ax1.set_xticks(x + 0.5)
-#ax1.set_xticklabels(type_chart.row_names, rotation='vertical')
+ax1.set_xticks(x + 0.5)
+ax1.set_xticklabels(['' for _ in x])
 ax1.set_title('Attacker')
 
-ax2.bar(x, numpy.log(balance_result.col_handicaps), color = colors)
+ax2.bar(x + bar_offset, numpy.log(balance_result.col_handicaps), bar_width, color = colors)
 ax2.set_xticks(x + 0.5)
 ax2.set_xticklabels(type_chart.col_names, rotation='vertical')
 ax2.set_title('Defender')
